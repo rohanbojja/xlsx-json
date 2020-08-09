7b08c80635d68a4dd70c7487592a42a2ea1dc81e
Rohan Bojja <rohanbojja@outlook.com>
2020-08-09 06:26:33 +0530

    Created XlsxToJsonService and cleaned up code.

--- src/main/java/com/rohanbojja/xlsxjson/models/Request.java
+++ src/main/java/com/rohanbojja/xlsxjson/models/Request.java
@@ -4,11 +4,21 @@ public class Request {
     String file_path;
     String file_name;
     String result_directory;
+    Boolean prettyPrinting;
 
-    public Request(String file_path, String file_name, String result_directory) {
+    public Request(String file_path, String file_name, String result_directory, Boolean prettyPrinting) {
         this.file_path = file_path;
         this.file_name = file_name;
         this.result_directory = result_directory;
+        this.prettyPrinting = prettyPrinting;
+    }
+
+    public Boolean getPrettyPrinting() {
+        return prettyPrinting;
+    }
+
+    public void setPrettyPrinting(Boolean prettyPrinting) {
+        this.prettyPrinting = prettyPrinting;
     }
 
     public String getFile_path() {
